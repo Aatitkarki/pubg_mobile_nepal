2b976f11d78ed5f4c6e57a79ef268b6c3ee03a5d
sachin-dahal <sachindahal2001@gmail.com>
2020-10-22 13:09:30 +0545

    home page ui setup

--- lib/modules/features/home_screen/view/home_screen_page.dart
+++ lib/modules/features/home_screen/view/home_screen_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:pubg_mobile_nepal/common/ui/ui_helper.dart';
+import 'package:pubg_mobile_nepal/modules/features/home_screen/widget/home_menu_widget.dart';
+import 'package:pubg_mobile_nepal/modules/features/welcome/view/welcome_page.dart';
 import 'package:pubg_mobile_nepal/theme/colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,82 +14,149 @@ class HomeScreen extends StatelessWidget {
     print("this is height $height");
     print("this is width $width");
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Flexible(
-              child: FractionallySizedBox(
-                  heightFactor: 40,
-                  widthFactor: width,
-                  child: Container(
-                    color: Colors.green,
-                  )
-                  //             Container(
-                  //   width: Get.width,
-                  //   color: kBackgroundColor,
-                  //   padding: EdgeInsets.only(top: 20, bottom: 10),
-                  //   child: Column(
-                  //     mainAxisAlignment: MainAxisAlignment.center,
-                  //     children: [
-                  //       Text(
-                  //         "WELCOME TO PUBG MOBILE NEPAL",
-                  //         style: Theme.of(context)
-                  //             .textTheme
-                  //             .bodyText2
-                  //             .copyWith(color: kPrimaryTextColor),
-                  //       ),
-                  //       Text("Himanshu Marasini")
-                  //     ],
-                  //   ),
-                  // ),
-                  ),
-            ),
-            Container(
-              height: 8,
-              width: double.infinity,
-              color: kSecondaryBackgroundColor,
-            ),
-            SingleChildScrollView(
-              child: Container(
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [menuWithIcon(context), menuWithIcon(context)],
-                    ),
-                    Row(
-                      children: [menuWithIcon(context), menuWithIcon(context)],
-                    ),
-                    Row(
-                      children: [menuWithIcon(context), menuWithIcon(context)],
-                    ),
-                    Row(
-                      children: [menuWithIcon(context), menuWithIcon(context)],
-                    ),
-                    Row(
-                      children: [menuWithIcon(context), menuWithIcon(context)],
-                    ),
-                  ],
+      body: Column(
+        children: [
+          Container(
+            height: height * 0.18,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "WELCOME TO PUBG MOBILE NEPAL",
+                  style: Theme.of(context).textTheme.headline5.copyWith(
+                        fontSize: 20.0,
+                        color: kPrimaryTextColor,
+                      ),
                 ),
-              ),
-            )
-            // FractionallySizedBox(
-            //   heightFactor: ,
-            // )
-          ],
-        ),
+                Text(
+                  "Himanshu Marasani",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .copyWith(fontSize: 18.0),
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            thickness: 7.0,
+            color: kSecondaryBackgroundColor,
+          ),
+          Expanded(
+            child: ListView(
+              physics: BouncingScrollPhysics(),
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Wrap(
+                        spacing: Get.width * 0.09,
+                        runSpacing: Get.height * 0.06,
+                        children: [
+                          HomeMenu(
+                            title: "Dashboard",
+                            imgUrl: "assets/icons/dashboard.svg",
+                          ),
+                          HomeMenu(
+                            title: "Upcoming Matches",
+                            imgUrl: "assets/icons/dashboard.svg",
+                          ),
+                          HomeMenu(
+                            title: "Free Giveaway Matches",
+                            imgUrl: "assets/icons/dashboard.svg",
+                          ),
+                          HomeMenu(
+                            title: "Upcoming Tournaments",
+                            imgUrl: "assets/icons/dashboard.svg",
+                          ),
+                          HomeMenu(
+                            title: "Match Highlights",
+                            imgUrl: "assets/icons/multimedia-player.svg",
+                          ),
+                          HomeMenu(
+                            title: "Recent Winners",
+                            imgUrl: "assets/icons/trophy.svg",
+                          ),
+                          HomeMenu(
+                            title: "PUBGM Nepal Center",
+                            imgUrl: "assets/icons/cart.svg",
+                          ),
+                          HomeMenu(
+                            title: "Redeem Center",
+                            imgUrl: "assets/icons/reward icon.svg",
+                          ),
+                          HomeMenu(
+                            title: "Contact Us",
+                            imgUrl: "assets/icons/Contact Icon.svg",
+                          ),
+                          HomeMenu(
+                            title: "Settings",
+                            imgUrl: "assets/icons/setting Icon.svg",
+                          ),
+                        ],
+                      ),
+                      lHeightSpan,
+                      ButtonTheme(
+                        minWidth: width * 0.9,
+                        padding: mYPadding,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: RaisedButton(
+                          color: kSecondaryButtonColor,
+                          onPressed: () => Get.offAll(WelcomePage()),
+                          child: Text(
+                            "Log out",
+                            style:
+                                Theme.of(context).textTheme.bodyText2.copyWith(
+                                      color: kBackgroundColor,
+                                      fontSize: 20.0,
+                                    ),
+                          ),
+                        ),
+                      ),
+                      elHeightSpan,
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // FractionallySizedBox(
+          //   heightFactor: ,
+          // )
+        ],
       ),
     );
   }
-
-  Widget menuWithIcon(BuildContext context) {
-    return Container(
-      width: Get.width * 0.4,
-      decoration: BoxDecoration(color: kLightYellowColor),
-      child: Center(
-          child: Column(
-        children: [Icon(Icons.ac_unit), Text("Dashboard")],
-      )),
-    );
-  }
 }
+
+// Flexible(
+//   child: FractionallySizedBox(
+//       heightFactor: 40,
+//       widthFactor: width,
+//       child: Container(
+//         color: Colors.green,
+//       )
+//       //             Container(
+//       //   width: Get.width,
+//       //   color: kBackgroundColor,
+//       //   padding: EdgeInsets.only(top: 20, bottom: 10),
+//       //   child: Column(
+//       //     mainAxisAlignment: MainAxisAlignment.center,
+//       //     children: [
+//       //       Text(
+//       //         "WELCOME TO PUBG MOBILE NEPAL",
+//       //         style: Theme.of(context)
+//       //             .textTheme
+//       //             .bodyText2
+//       //             .copyWith(color: kPrimaryTextColor),
+//       //       ),
+//       //       Text("Himanshu Marasini")
+//       //     ],
+//       //   ),
+//       // ),
+//       ),
+// ),
---
+++ lib/modules/features/home_screen/widget/home_menu_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:pubg_mobile_nepal/common/ui/ui_helper.dart';
+import 'package:pubg_mobile_nepal/theme/colors.dart';
+
+class HomeMenu extends StatelessWidget {
+  final String title;
+  final String imgUrl;
+
+  HomeMenu({
+    this.title,
+    this.imgUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: Get.width * 0.4,
+      height: Get.height * 0.15,
+      decoration: BoxDecoration(
+        color: kLightYellowColor,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            imgUrl,
+            height: 30.0,
+          ),
+          eesHeightSpan,
+          esHeightSpan,
+          Text(
+            title,
+            style: Theme.of(context).textTheme.bodyText2.copyWith(
+                  color: kPrimaryTextColor,
+                  fontSize: 16.0,
+                ),
+          ),
+        ],
+      )),
+    );
+  }
+}
